3cdd9595548a87ed52690d66dad74457a49e6c45
Ashraful-Fuqha <120241035+Ashraful-Fuqha@users.noreply.github.com>
2023-12-14 20:15:26 +0530

    Testing

--- src/js/script.js
+++ src/js/script.js
@@ -6,13 +6,13 @@ function handleChange(){
     console.log("Changaa!")
 }
 
-// inputs.forEach(input => input.addEventListener('change', handleChange))
+inputs.forEach(input => input.addEventListener('change', handleChange))
 inputs.forEach(input => input.addEventListener('mousemove', handleChange))
 
 const inputss = document.querySelectorAll('input')
 
 inputss.forEach(input => {
-    input.addEventListener('change', () =>{
+    input.addEventListener('change', function () {
         const suffix = this.dataset.sizing || '';
         document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     })
